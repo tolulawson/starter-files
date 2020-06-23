3e189b3c6e691ca25c59e1e025bd057d97615c70
Tolu Lawson <lawsontolu1@gmail.com>
2020-06-23 07:24:22 +0100

    -- Build process refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,37 @@
-const gulp = require ('gulp');
-const sass = require ('gulp-sass');
-const autoprefixer = require ('gulp-autoprefixer');
-const uglify = require ('gulp-uglify');
-const babel = require ('gulp-babel');
-const htmlmin = require ('gulp-htmlmin');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const htmlmin = require('gulp-htmlmin');
 const image = require('gulp-image');
 const browsersync = require('browser-sync').create();
 const cleancss = require('gulp-clean-css');
+const jasmineBrowser = require('gulp-jasmine-browser');
+const sourcemaps = require('gulp-sourcemaps');
 
 function promisifyStream(stream) {
-    return new Promise(resolve => stream.on('end',resolve));
+  return new Promise((resolve) => stream.on('end', resolve));
 }
-
 
 gulp.task('sass', async function() {
   await promisifyStream(
-    gulp.src('app/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'))
+    gulp.src('src/scss/*.scss')
+        .pipe(sass({ouputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
   )
+});
+
+gulp.task('start-sass', function() {
+  return gulp.src('src/scss/*.scss')
+        .pipe(sass({ouputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        // .pipe(browsersync.stream())
 });
 
 gulp.task('cleancss', async function() {
   await promisifyStream(
-    gulp.src('app/css/*.css')
+    gulp.src('src/css/*.css')
       .pipe(cleancss({compatibility: 'ie8'}))
       .pipe(gulp.dest('dist/css'))
   )
@@ -31,7 +39,7 @@ gulp.task('cleancss', async function() {
 
 gulp.task('autoprefixer', async function() {
   await promisifyStream(
-      gulp.src('app/css/*.css', {base: './'})
+      gulp.src('src/css/*.css', {base: './'})
         .pipe(autoprefixer('last 2 versions'))
         .pipe(gulp.dest('./'))
   )
@@ -39,7 +47,7 @@ gulp.task('autoprefixer', async function() {
 
 gulp.task('babel', async function() {
   await promisifyStream(
-    gulp.src('app/js/*.js')
+    gulp.src('src/js/*.js')
       .pipe(babel({
           presets: [
           ['@babel/env', {
@@ -54,14 +62,16 @@ gulp.task('babel', async function() {
 gulp.task('uglify', async function() {
   await promisifyStream(
     gulp.src('dist/js/*.js', {base: './'})
+      // .pipe(sourcemaps.init())
       .pipe(uglify())
+      // .pipe(sourcemaps.write('maps'))
       .pipe(gulp.dest('./'))
   )
 })
 
 gulp.task('htmlmin', async function() {
   await promisifyStream(
-    gulp.src('app/**/*.html')
+    gulp.src('src/**/*.html')
       .pipe(htmlmin({ collapseWhitespace: true }))
       .pipe(gulp.dest('dist'))
   )
@@ -69,23 +79,28 @@ gulp.task('htmlmin', async function() {
 
 gulp.task('image', async function () {
   await promisifyStream(
-    gulp.src('app/img/*')
+    gulp.src('src/img/*')
     .pipe(image())
     .pipe(gulp.dest('dist/img'))
   )
 })
 
-gulp.task('browsersync', function() {
-    browsersync.init({
-        server: {
-            baseDir: "app"
-        }
-    });
+gulp.task('tests', async function() {
+    gulp
+        .src('src/js/app.js')
+        .pipe(jasmineBrowser.specRunner())
+        .pipe(jasmineBrowser.server({ port: 3001 }));
 });
 
-gulp.task('start', function() {
-  gulp.watch('app/scss/*.scss', {ignoreInitial: false}, gulp.series('sass', 'autoprefixer'));
-  (gulp.series('browsersync'))();
+gulp.task('start', async function() {
+  // (gulp.series('browsersync'))();
+  browsersync.init({
+      server:  {
+        baseDir : "src"
+      }
+  });
+  gulp.watch('src/scss/*.scss', {ignoreInitial: false}, gulp.series('start-sass')).on('change', browsersync.reload);
+  gulp.watch(['src/*.html', 'src/js/*.js']).on('change', browsersync.reload);
 })
 
 gulp.task('build', async function() {
